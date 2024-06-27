2f33197e7bdd6a903a70e636ec5bc9ceddf62711
tmartin42 <thibaultmartin42@gmail.com>
2024-06-27 22:24:32 +0200

    add working cooldown timer

--- src/components/playerGamePage/SummonerSpell.vue
+++ src/components/playerGamePage/SummonerSpell.vue
@@ -1,9 +1,9 @@
 <template>
 
-<div class="summoner-spell">
+<div class="summoner-spell" @click="setOnCd">
     <img :src="spellIcon" />
-    <div class="spell-cooldown" v-if="!!spellCd">
-        {{ spellCd }}
+    <div class="spell-cooldown" v-if="!!currentSpellCd">
+        {{ displaySpellCd(currentSpellCd) }}
     </div>
 </div>
 
@@ -11,33 +11,56 @@
 
 <script>
 export default {
-  name: "SummonerSpell",
-  props: {
-    spellInfo: Object,
-  },
-
-  computed: {
-    spellIcon() {
-        return `https://ddragon.leagueoflegends.com/cdn/14.13.1/img/spell/${this.spellInfo.image.full}`
+    name: "SummonerSpell",
+    props: {
+        spellInfo: Object,
     },
-    spellCd() {
 
-        let timeString = ''
-        if (this.spellInfo) {
-        const minutes = this.spellInfo.cooldown[0] / 60;
-        const seconds = this.spellInfo.cooldown[0] % 60;
-        if (minutes >= 1)
-            timeString += `${minutes}:`
-        if (seconds < 10)
-            timeString += `0${seconds}`
-        if (seconds >= 10)
-            timeString += `${seconds}`
-        if (timeString === "00")
-            return ''
+    data() {
+        return {
+            currentSpellCd: 0,
         }
-        return timeString
+    },
+    computed: {
+        spellIcon() {
+            return `https://ddragon.leagueoflegends.com/cdn/14.13.1/img/spell/${this.spellInfo.image.full}`
+        },
+        spellCd() {
+            return this.spellInfo.cooldown[0]
+        },
+    },
+    methods: {
+        setOnCd(){
+            if (this.currentSpellCd <= 0) {
+                this.currentSpellCd = this.spellCd;
+                const cdInterval = setInterval(() => {
+                    this.currentSpellCd--;
+                    
+                    if (this.currentSpellCd <= 0) 
+                        clearInterval(cdInterval)
+        
+                }, 1000)
+            }  
+        },
+
+        displaySpellCd(spellCd) {
+
+            let timeString = ''
+            if (spellCd) {
+            const minutes = Math.floor(spellCd / 60);
+            const seconds = spellCd % 60;
+            if (minutes >= 1)
+                timeString += `${minutes}:`
+            if (seconds < 10)
+                timeString += `0${seconds}`
+            if (seconds >= 10)
+                timeString += `${seconds}`
+            if (timeString === "00")
+                return ''
+            }
+            return timeString
+        }
     }
-  }
 };
 </script>
 <style scoped lang="scss">
@@ -47,6 +70,7 @@ export default {
     margin-left: 10px;
     margin-right: 10px;
     position: relative;
+    cursor: pointer;
 }
 
 .spell-cooldown {
@@ -58,5 +82,6 @@ export default {
     position: absolute;
     top: 0;
     background: #101010d4;
+    box-shadow: inset 0px 0px 0px 4px #930606;
 }
 </style>
