53b66090e38d8c5c394e371424e3fe77c4784b28
tmartin42 <thibaultmartin42@gmail.com>
2024-06-27 20:48:59 +0200

    add some style to champion list

--- src/components/playerGamePage/PlayerInfo.vue
+++ src/components/playerGamePage/PlayerInfo.vue
@@ -1,18 +1,15 @@
 <template>
 
-  <div>
-    <div class="champion-container">
-      <img :src="championSplash"/>
+  <div class="champion-container">
+    <img :src="championSplash"/>
 
-      {{ championInfo.name }}
-
-      <div class="summoner-spells-container">
-        <SummonerSpell :spellInfo="playerInfo.spell1Info" />
-        <SummonerSpell :spellInfo="playerInfo.spell2Info" />
-      </div>
+    <div class="champion-name">
+        {{ championInfo.name }}
     </div>
-
-
+    <div class="summoner-spells-container">
+      <SummonerSpell :spellInfo="playerInfo.spell1Info" />
+      <SummonerSpell :spellInfo="playerInfo.spell2Info" />
+    </div>
   </div>
 
 
@@ -44,4 +41,33 @@ export default {
 };
 </script>
 <style scoped lang="scss">
+
+.champion-container {
+    display: inline-block;
+    position: relative;
+}
+.champion-name {
+    font-size: 20px;
+    color: white;
+    height: 100px;
+    width: 100%;
+    line-height: 67px;
+    position: absolute;
+    bottom: 71px;
+    background: rgba(16, 16, 16, 0.831372549);
+    font-size: 20px;
+    color: white;
+    height: 110px;
+    width: 100%;
+    line-height: 100px;
+    position: absolute;
+    bottom: 70px;
+    background: rgba(16, 16, 16, 0.831372549);
+    background: rgb(2, 0, 36);
+    background: linear-gradient(90deg, rgba(2, 0, 36, 1) 0%, rgba(0, 0, 0, 1) 100%);
+    background: rgb(0, 0, 0);
+    background: linear-gradient(0deg, rgba(0, 0, 0, 1) 20%, rgba(0, 0, 0, 0) 110%);
+}
+
+
 </style>
--- src/components/playerGamePage/SummonerSpell.vue
+++ src/components/playerGamePage/SummonerSpell.vue
@@ -1,8 +1,8 @@
 <template>
 
-<div>
+<div class="summoner-spell">
     <img :src="spellIcon" />
-    <div>
+    <div class="spell-cooldown" v-if="!!spellCd">
         {{ spellCd }}
     </div>
 </div>
@@ -23,13 +23,37 @@ export default {
     spellCd() {
         const minutes = this.spellInfo.cooldown[0] / 60;
         const seconds = this.spellInfo.cooldown[0] % 60;
-        if (minutes) {
-            return `${minutes}min ${seconds}`
-        }
-        return `${seconds}`
+        let timeString = ''
+        if (minutes >= 1)
+            timeString += `${minutes}:`
+        if (seconds < 10)
+            timeString += `0${seconds}`
+        if (seconds >= 10)
+            timeString += `${seconds}`
+        if (timeString === "00")
+            return ''
+        return timeString
     }
   }
 };
 </script>
 <style scoped lang="scss">
+
+.summoner-spell {
+    display: inline-block;
+    margin-left: 10px;
+    margin-right: 10px;
+    position: relative;
+}
+
+.spell-cooldown {
+    font-size: 20px;
+    color: white;
+    height: 100%;
+    width: 100%;
+    line-height: 67px;
+    position: absolute;
+    top: 0;
+    background: #101010d4;
+}
 </style>
